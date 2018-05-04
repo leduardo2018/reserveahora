003972dec1bdffa6e1b55cd72a4660ae49cbbcd1
leduardo2018 <dev1@marketinghotelero.co>
2018-05-04 11:57:08 -0500

    verify from QA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ReserveAhora.com
+                    ReserveAhora.com desde QA
                 </div>
 
                 >
